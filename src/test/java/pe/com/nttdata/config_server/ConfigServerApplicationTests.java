c15547b1cdd3198c963d6ee681d48313db844b5a
Mauricio Cc <mauricioccasani50@gmail.com>
2024-11-21 17:42:43 -0500

    Producto inicial

---
+++ src/test/java/pe/com/nttdata/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.nttdata.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
